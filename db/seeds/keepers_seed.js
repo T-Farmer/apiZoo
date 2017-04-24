d9ebfb06ce6566d99ac0161ca281c793827263e0
T-Farmer <terrillfarmer@gmail.com>
2017-04-24 15:23:52 -0500

    seeding for zookepers

---
+++ db/seeds/keepers_seed.js
@@ -0,0 +1,41 @@
+//seeding for zookeepers
+exports.seed = function(knex, Promise) {
+  return knex('keepers').del()
+  .then(function () {
+    return Promise.all([
+      knex('keepers').insert({
+        name: 'Scott',
+      }),
+      knex('keepers').insert({
+        name: 'Joe',
+      }),
+      knex('keepers').insert({
+        name: 'Caitlin',
+      }),
+      knex('keepers').insert({
+        name: 'Callan',
+      }),
+      knex('keepers').insert({
+        name: 'Dom',
+      }),
+      knex('keepers').insert({
+        name: 'Matt',
+      }),
+      knex('keepers').insert({
+        name: 'Jack',
+      }),
+      knex('keepers').insert({
+        name: 'John',
+      }),
+      knex('keepers').insert({
+        name: 'Steve',
+      }),
+      knex('keepers').insert({
+        name: 'Jessica',
+      }),
+      knex('keepers').insert({
+        name: 'Brenda',
+      }),
+      })
+    ]);
+  };
