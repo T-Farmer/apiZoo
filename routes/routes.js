02170588b9d9b72b50ceafad5c99668fd5e034f4
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-24 14:51:08 -0500

    added gets/posts to routes.js

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const  { Router } = require('express');
+const router = Router();
+
+// const{ all of the methods from the controllers HERE } = require('CONTROLLER')
+
+
+// router.get('/animals', METHOD TO CALL ALL ANIMALS);
+// router.post('/animals/new', METHOD TO POST NEW ANIMAL);
+// router.get('/keepers', METHOD TO GET ALL KEEPERS);
+// router.post('/keepers/new', METHOD TO POST A NEW KEEPER);
+// router.get('animals/:id', METHOD TO GET ALL ANIMALS FOR A KEEPER);
+// router.get('keepers/:id', METHOD TO GET ALL KEEPERS FOR A ANIMAL);
+
+
+module.exports = router;
