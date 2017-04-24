7a514b37d473ca9ce9ab81294a2f0d414c8c55d7
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-24 14:54:52 -0500

    added delete/patch routes left them commented out for the time being

--- routes/routes.js
+++ routes/routes.js
@@ -12,6 +12,11 @@ const router = Router();
 // router.post('/keepers/new', METHOD TO POST A NEW KEEPER);
 // router.get('animals/:id', METHOD TO GET ALL ANIMALS FOR A KEEPER);
 // router.get('keepers/:id', METHOD TO GET ALL KEEPERS FOR A ANIMAL);
+// router.delete('keepers/:id', METHOD TO DELETE A KEEPER BY ID)
+// router.delete('/animals/:id', METHOD TO DELETE AN ANIMAL);
+// router.patch('/animals/:new', METHOD TO PATCH ANIMAL INFO);
+// router.patch('/keepers/:new', METHOD TO PATCH KEEPERS INFO);
+//
 
 
 module.exports = router;
