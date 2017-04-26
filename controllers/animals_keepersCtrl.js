f7bd00c361779b82ad0ac4da3b16d4f693ed4810
TSturtledove <roberttortorelli@yahoo.com>
2017-04-26 13:51:27 -0500

    finished fixing merge errors

--- controllers/animals_keepersCtrl.js
+++ controllers/animals_keepersCtrl.js
@@ -14,13 +14,9 @@ module.exports.getKeepersForAnimal = ( {params: { id } }, res, next) => {
   })
 }
 
-<<<<<<< HEAD
-module.exports.getAnimalsForKeeper = ( { params: { id } }, res, next ) => {
-  AKeepers.getAnimalsforKeeper(id)
-=======
+
 module.exports.getAnimalsforKeeper = ( { params: { id } }, res, next ) => {
   Animals_Keepers.getAnimalsforKeeper(id)
->>>>>>> edaf22250d80acb9cd5808ecbb51e40449b57c51
   .then( (animalArr) => {
     res.status(200).json(animalArr)
     .catch( (err) => {
@@ -33,11 +29,8 @@ module.exports.getAnimalsforKeeper = ( { params: { id } }, res, next ) => {
 module.exports.joinDeleteKeeper = ( { params: { id} }, res, next ) => {
   Animals_Keepers.deleteKeeper(id)
   .then( (res) => {
-<<<<<<< HEAD
     res.status(200).json({msg: 'keeper deleted from join table'})
-=======
-    res.status(200).json({'msg': 'keeper deleted from join table'})
->>>>>>> edaf22250d80acb9cd5808ecbb51e40449b57c51
+
   })
   .catch( (err) => {
     console.log("err in joinDeleteKeeper")
@@ -48,11 +41,7 @@ module.exports.joinDeleteKeeper = ( { params: { id} }, res, next ) => {
 module.exports.joinDeleteAnimal = ( { params: { id} }, res, next ) => {
   Animals_Keepers.deleteAnimal(id)
   .then( (res) => {
-<<<<<<< HEAD
     res.status(200).json({msg: 'animal deleted from join table'})
-=======
-    res.status(200).json({'msg': 'animal deleted from join table'})
->>>>>>> edaf22250d80acb9cd5808ecbb51e40449b57c51
   })
   .catch( (err) => {
     console.log("err in joindeleteAnimal")
