47f3e7c7aa75fdcb1b1ca9b818d43230c1851eab
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-26 09:17:16 -0500

    post and delete methods in animal controller

--- controllers/animalsCtrl.js
+++ controllers/animalsCtrl.js
@@ -13,3 +13,20 @@ module.exports.getAnimals = (req, res, next)  =>  {
   .then(Animals =>  res.status(200).json(Animals))
   .catch((err)  => { return next(err)})
 }
+
+module.exports.postNewAnimal = ({ body }, res, next)  =>  {
+  Animals.forge(body)
+  .then(Animals =>  res.status(201).json({msg: 'You posted an animal!'}))
+  .catch((err)  => { return next(err)})
+}
+
+module.exports.deleteShow = ({ params: { id }}, res, next) => {
+  Animals.forge({id})
+  .destroy()
+  .then( (animal) => {
+    res.status(200).json(animal)
+  })
+  .catch( (err) => {
+    console.log('deleteshow err', err)
+  })
+}
