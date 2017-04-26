d229b799989f149b2c3cc8c61d0d2ea943c52330
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-26 10:33:09 -0500

    inserted methods from keeper controller into routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,19 +4,19 @@ const  { Router } = require('express');
 const router = Router();
 
 const { getAnimals, getAnimal, postNewAnimal, deleteAnimal, animalEdit  } = require('../controllers/animalsCtrl')
-const { getKeeper, getKeepers  } = require('../controllers/keepersCtrl')
+const { getKeeper, getKeepers, keeperEdit, deleteKeeper, postNewKeeper } = require('../controllers/keepersCtrl')
 
 
 router.get('/animals', getAnimals);
 router.post('/animals/new', postNewAnimal);
 router.get('/keepers', getKeepers);
-// router.post('/keepers/new', METHOD TO POST A NEW KEEPER);
+router.post('/keepers/new', postNewKeeper );
 // router.get('animals/:id', METHOD TO GET ALL ANIMALS FOR A KEEPER);
 // router.get('keepers/:id', METHOD TO GET ALL KEEPERS FOR A ANIMAL);
-// router.delete('keepers/:id', METHOD TO DELETE A KEEPER BY ID)
+router.delete('keepers/:id', deleteKeeper)
 router.delete('/animals/:id', deleteAnimal);
 router.patch('/animals/:new', animalEdit);
-// router.patch('/keepers/:new', METHOD TO PATCH KEEPERS INFO);
+router.patch('/keepers/:new', keeperEdit);
 
 
 
