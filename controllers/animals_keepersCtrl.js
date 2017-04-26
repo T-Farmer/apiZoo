737113109b43818452a23289ec70fe550e09485d
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-26 11:50:22 -0500

    added controller methods to animal_keepers controller deleteanimal/keeper

--- controllers/animals_keepersCtrl.js
+++ controllers/animals_keepersCtrl.js
@@ -23,3 +23,25 @@ module.exports.getAnimalsforKeeper = ( { params: { id } }, res, next ) => {
     })
   })
 }
+
+module.exports.joinDeleteKeeper = ( { params: { id} }, res, next ) => {
+  AKeepers.deleteKeeper(id)
+  .then( (res) => {
+    res.status(200).json('msg': 'keeper deleted from join table')
+  })
+  .catch( (err) => {
+    console.log("err in joinDeleteKeeper")
+    return next(err)
+  })
+}
+
+module.exports.joinDeleteAnimal = ( { params: { id} }, res, next ) => {
+  AKeepers.deleteAnimal(id)
+  .then( (res) => {
+    res.status(200).json('msg': 'animal deleted from join table')
+  })
+  .catch( (err) => {
+    console.log("err in joindeleteAnimal")
+    return next(err)
+  })
+}
